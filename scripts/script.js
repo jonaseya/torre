d2d58c24a37aacab3c159d6a7bd4d32ac4bab590
YONAS <Yoni442000@gmail.com>
2021-11-12 12:46:48 +0300

    improvision of skills details

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,10 @@
-var element = document.getElementById("software-dev").style.display = "none";
+function myFunction(divid) {
 
-document.getElementById("revealer").addEventListener('click', function() {
-    var x = document.getElementById("software-dev");
-    if (x.style.display === "none") {
+    var x = document.getElementById(divid);
+
+    if (x.style.display == "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
-});
-fetch('https://search.torre.co/opportunities/_search/?', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name: 'search' })
-    })
-    .then(res => {
-        return res.json()
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
+}
