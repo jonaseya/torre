b9c472add2e1079f419ecf2acff983df9f7efdbf
YONAS <Yoni442000@gmail.com>
2021-11-14 23:27:28 +0300

    Introduction of Search bar +  other  minor issues improved

--- scripts/script.js
+++ scripts/script.js
@@ -7,80 +7,68 @@ function myFunction(divid) {
     } else {
         x.style.display = "none";
     }
-};
-
-function searchData() {
-    fetch('https://search.torre.co/opportunities/_search/?', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: 'Alexander',
-                professionalHeadline: 'Developer'
-            })
-        }).then(res => {
-            if (!res.ok) {
-                throw Error('Error Accured');
-            }
-            return res.json();
-
-        })
-        .then(data => {
-            console.log(data)
-        }).catch(error => {
-            console.log(error);
-        });
 }
 
 
 
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
 
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
+const div = document.getElementById("jobs"),
+    url = "";
+fetch(url).
+then(response => {
+    return response.json();
+}).
+then(data => {
+    let jobs = data.jobs;
 
+    let jobsLengthTitle = document.querySelector(".job-length");
+    if (jobs.length === 0) {
+        jobsLengthTitle.innerHTML = "Sorry no jobs to show right now";
+    } else {
+        jobsLengthTitle.innerHTML = `<h3>There are ${jobs.length} availible jobs right now.</h3>`;
+    }
 
+    jobs.forEach(job => {
+        let output = '<div class="container">';
 
-// function fetchData() {
-//     fetch('https://search.torre.co/people/_search/?')
-//         .then(response => {
-//             if (response.ok) {
-//                 throw Error('Error Accured');
-//             }
-//             return response.json();
-//         }).then(data => {
-//             console.log(data.data);
-//             const html = data.data
-//                 .map(user => {
-//                     return `
-//                         <div class="user">
-//                         <p><img src="${user.picture}" alt="${user.name}" /></p>
-//                         <p>Name : ${user.name}</p>
-//                         <p> professionalHeadline: ${user.professionalHeadline}</p>
-//                     </div>
-//                     `;
-//                 })
-//                 .join("");
-//             document.querySelector("#app").insertAdjacentElement("afterbegin", html);
-//         })
-//         .catch(error => {
-//             console.log(error);
-//         });
-// }
-// fetchData();
+        function truncateString(myString, limit) {
+            const shortened = myString.indexOf(" ", limit);
+            if (shortened == -1) return myString;
+            return (
+                myString.substring(0, shortened) +
+                `<a class="read-more" href="${job.url}" target="_blank"> Read More ...</a>`);
 
+        }
+        jobs.forEach(job => {
+            output += `
+        <div class="card">
+          <div class="card-inner">
+            <h1>${job.company_name}</h1>
+            <span>${job.featured_text}</span>
+            <p>${job.title}</p>
+            <p>${job.remote}</p>
+            <div class="term"> ${job.term}</div>
+            <p class="space-b-sm">${truncateString(job.description, 250)}</p>
+            <a class="btn btn-main" href=${
+      job.url
+      } target="_blank">Apply Now</a>
+          </div>
+        </div>
+      `;
+        });
 
-// var url = "https://search.torre.co/opportunities/_search/?";
-
-// var xhr = new XMLHttpRequest();
-// xhr.open("POST", url);
-
-// xhr.setRequestHeader("Content-Type", "application/json");
-// xhr.setRequestHeader("Content-Length", "0");
-
-// xhr.onreadystatechange = function() {
-//     if (xhr.readyState === 4) {
-//         console.log(xhr.status);
-//         console.log(xhr.responseText);
-//     }
-// };
-
-// xhr.send();
+        if (job.remote === true) {
+            job.remote = "Location: Remote";
+        } else {
+            job.remote = "";
+        }
+        document.getElementById("jobs").innerHTML = output;
+    });
+}).
+catch(error => console.log(error));
